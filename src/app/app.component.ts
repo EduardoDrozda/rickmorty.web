bda54bac8d10e1062c1012f2edf0fe8ad90abd3c
Eduardo Drozda <Edu_Drozda@hotmail.com>
2024-06-10 14:12:16 -0300

    Config font awesome icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontsModule } from '@shared/modules/fonts';
 
 @Component({
   selector: 'rickmorty-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
