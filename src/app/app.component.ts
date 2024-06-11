a76289a2ea6fba46c738f22da2be5eefe5d8fefa
Eduardo Drozda <Edu_Drozda@hotmail.com>
2024-06-10 23:33:13 -0300

    Create page title class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@shared/components';
 
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'rickmorty-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
