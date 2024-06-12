0fa15e53ddac0e8a58ff1f542befd1d5418cd25a
Eduardo Drozda <Edu_Drozda@hotmail.com>
2024-06-12 11:30:04 -0300

    Fix: Lint issues

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
