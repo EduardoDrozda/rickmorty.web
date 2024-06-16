c615d9db04ae259d433f346c95a35b2b1178d99d
Eduardo Drozda <Edu_Drozda@hotmail.com>
2024-06-16 15:30:24 -0300

    test: create test to characters-list-grid

--- src/app/modules/characters/shared/components/characters-list-grid/characters-list-grid.component.spec.ts
+++ src/app/modules/characters/shared/components/characters-list-grid/characters-list-grid.component.spec.ts
@@ -8,9 +8,8 @@ describe('CharactersListGridComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CharactersListGridComponent]
-    })
-    .compileComponents();
+      imports: [CharactersListGridComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CharactersListGridComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,30 @@ describe('CharactersListGridComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit favorite event', () => {
+    jest.spyOn(component.favoriteEvent, 'emit');
+    component.onFavorite(1);
+    expect(component.favoriteEvent.emit).toHaveBeenCalledWith(1);
+  });
+
+  it('should emit scroll event', () => {
+    component.enableInfiniteScroll = true;
+    jest.spyOn(component.scrollEndEvent, 'emit');
+    component.onScrollEnd();
+    expect(component.scrollEndEvent.emit).toHaveBeenCalled();
+  });
+
+  it('should not emit scroll event', () => {
+    component.enableInfiniteScroll = false;
+    jest.spyOn(component.scrollEndEvent, 'emit');
+    component.onScrollEnd();
+    expect(component.scrollEndEvent.emit).not.toHaveBeenCalled();
+  });
+
+  it('should track by id', () => {
+    const character = { id: 1 } as any;
+    expect(component.trackByFn(1, character)).toBe(1);
   });
 });
