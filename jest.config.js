8365a3d4cb902d2579db1d7f65607f8f859f09a0
Eduardo Drozda <Edu_Drozda@hotmail.com>
2024-06-16 09:37:14 -0300

    feat: config jest

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+    "<rootDir>/src/test.ts",
+  ],
+  moduleNameMapper: {
+    "@modules/(.*)": "<rootDir>/src/app/modules/$1",
+    "@core/(.*)": "<rootDir>/src/app/core/$1",
+    "@shared/(.*)": "<rootDir>/src/app/shared/$1",
+    "@enviroments/(.*)": "<rootDir>/src/environments/$1",
+    "@core": "<rootDir>/src/app/core",
+  },
+  collectCoverage: true,
+  coverageReporters: ["html", "lcov"],
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/app/**/*.ts",
+    "!src/app/**/*.module.ts",
+    "!src/app/**/index.ts",
+    "!src/app/**/*.interface.ts",
+    "!src/app/**/*.model.ts",
+    "!src/app/**/*.enum",
+  ],
+
+};
