891d63c5fee7718652af33452796f9a178a2d37a
Eduardo Drozda <Edu_Drozda@hotmail.com>
2024-06-16 17:58:22 -0300

    test: create unit tests to fontComponent

--- src/app/modules/characters/pages/characters-favorites/layout/characters-favorites-layout/characters-favorites-layout.component.spec.ts
+++ src/app/modules/characters/pages/characters-favorites/layout/characters-favorites-layout/characters-favorites-layout.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CharactersFavoritesLayoutComponent } from './characters-favorites-layout.component';
+import { By } from '@angular/platform-browser';
+import { CHARACTERS_MOCK_VIEW_MODEL } from '@shared/mocks/characters';
+import { ICharacterViewModel } from '@core/view-models';
 
 describe('CharactersFavoritesLayoutComponent', () => {
   let component: CharactersFavoritesLayoutComponent;
@@ -19,5 +22,17 @@ describe('CharactersFavoritesLayoutComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit favoriteEvent when onFavorite is called', () => {
+    jest.spyOn(component.favoriteEvent, 'emit');
+    component.onFavorite(1);
+    expect(component.favoriteEvent.emit).toHaveBeenCalledWith(1);
+  });
+
+  it('should emit navigateEvent when onNavigate is called', () => {
+    jest.spyOn(component.navigateEvent, 'emit');
+    component.onNavigate();
+    expect(component.navigateEvent.emit).toHaveBeenCalled();
   });
 });
--- src/app/shared/components/font/font.component.spec.ts
+++ src/app/shared/components/font/font.component.spec.ts
@@ -1,18 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FontComponent } from './font.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rickmorty-font-mock',
+  standalone: true,
+  imports: [FontComponent],
+  template: '<rickmorty-font id="mock-id" icon="house" color="white" />'
+}) class FontComponentTest {}
 
 describe('FontComponent', () => {
-  let component: FontComponent;
-  let fixture: ComponentFixture<FontComponent>;
+  let component: FontComponentTest;
+  let fixture: ComponentFixture<FontComponentTest>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FontComponent]
+      imports: [FontComponentTest, FontComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FontComponent);
+    fixture = TestBed.createComponent(FontComponentTest);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
